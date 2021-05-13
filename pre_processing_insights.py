bfa69de83d58efb60226bedc66a63cc5bc704da3
Vikrant7981 <70353912+Vikrant7981@users.noreply.github.com>
2021-05-13 18:35:10 +0530

    Update pre_processing_insights.py

--- pre_processing_insights.py
+++ pre_processing_insights.py
@@ -1,1 +1,36 @@
 
+# imports
+import os
+import pandas as pd
+import numpy as np
+import seaborn as sns
+sns.set(color_codes=True)
+import matplotlib.pyplot as plt
+
+
+data_dir = 'Here place the location of unzipped BearingData Set2 folder'
+merged_data = pd.DataFrame()
+
+for filename in os.listdir(data_dir):
+
+    dataset=pd.read_csv(os.path.join(data_dir, filename), sep='\t')
+    dataset_mean_abs = np.array(dataset.abs().mean())
+    dataset_mean_abs = pd.DataFrame(dataset_mean_abs.reshape(1,4))
+    dataset_mean_abs.index = [filename]
+    merged_data = merged_data.append(dataset_mean_abs)
+
+merged_data.columns = ['Bearing 1','Bearing 2','Bearing 3','Bearing 4']
+merged_data.index = pd.to_datetime(merged_data.index, format='%Y.%m.%d.%H.%M.%S')
+merged_data = merged_data.sort_index()
+merged_data.to_csv('./dataset/dataset_Bearing_rough.csv')
+merged_data.head()
+
+
+dataset_train = merged_data['2004-02-12 11:02:39':'2004-02-13 23:52:39']
+dataset_test = merged_data['2004-02-13 23:52:39':]
+dataset_train.plot(figsize = (12,6))
+plt.show()
+dataset_test.plot(figsize = (12,6))
+plt.show()
+
+
